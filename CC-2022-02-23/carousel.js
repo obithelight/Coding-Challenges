ab141e1bc8cb21f8da28ce6de7e0f5b32bf0ccea
Kenneth Onu <kennetho.onu@gmail.com>
2022-03-10 16:29:33 +0800

    Number of turns of carousel

---
+++ CC-2022-02-23/carousel.js
@@ -0,0 +1,6 @@
+/*Carousel
+Write a program that launches a carousel for 10 turns, showing the turn number each time.
+
+When it's done, improve it so that the number of turns is given by the user.
+*/
+
