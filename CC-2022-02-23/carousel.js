80194b5272db526c46312c29eb00b0d997c4fe05
Kenneth Onu <kennetho.onu@gmail.com>
2022-03-12 16:34:51 +0800

    Number of turns of carousel

--- CC-2022-02-23/carousel.js
+++ CC-2022-02-23/carousel.js
@@ -4,7 +4,9 @@ Write a program that launches a carousel for 10 turns, showing the turn number e
 When it's done, improve it so that the number of turns is given by the user.
 */
 
-//Solution 1
+//Solution 
 for(let i = 1; i <= 10; i++){
   console.log(i) 
 }
+
+//When it's done, improve it so that the number of turns is given by the user
