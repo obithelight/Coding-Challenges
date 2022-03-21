db1a7b10810946aa4eb2c0644a07a98961f6e73c
Kenneth Onu <kennetho.onu@gmail.com>
2022-03-21 19:26:54 +0800

    Input validation

--- CC-2022-02-23/validation.js
+++ CC-2022-02-23/validation.js
@@ -3,4 +3,6 @@ Input validation
 Write a program that continues to ask the user for a number until the entered number is less than or equal to 100.
 
 When you are done with the above, improve the program so that the terminating number is between 50 and 100.
-*/
+*/ 
+
+//Solution
