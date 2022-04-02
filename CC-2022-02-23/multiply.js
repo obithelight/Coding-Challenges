66d53551afe58101e8488555b99f62ef5eaef534
Kenneth Onu <kennetho.onu@gmail.com>
2022-04-02 22:30:13 +0800

    Multiplication table of integers

--- CC-2022-02-23/multiply.js
+++ CC-2022-02-23/multiply.js
@@ -4,3 +4,7 @@ Write a program that asks the user for a number, then shows the multiplication t
 When you are done, improve the program so it only accepts numbers between 2 and 9 (use the previous exercise as a blueprint).*/
 
 //Solution 
+let userNumber = +prompt("Please enter a number") 
+for(let i = 1; i <= userNumber; i++){
+  console.log(`${i} * ${userInput} = ${i * userInput}`)
+}
