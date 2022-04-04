173c62f5d3407ce88298064e6c15c3f8b5f5c64e
Kenneth Onu <kennetho.onu@gmail.com>
2022-04-04 22:34:20 +0800

    Multiplication table of integers

--- CC-2022-02-23/multiply.js
+++ CC-2022-02-23/multiply.js
@@ -6,5 +6,16 @@ When you are done, improve the program so it only accepts numbers between 2 and 
 //Solution 
 let userNumber = +prompt("Please enter a number") 
 for(let i = 1; i <= userNumber; i++){
+  console.log(`${i} * ${userNumber} = ${i * userNumber}`)
+}
+
+//Improve the program so it only accepts numbers between 2 and 9 (use the previous exercise as a blueprint).
+
+let userInput = 0;
+while (userInput < 2 || userInput > 9) {
+  userInput = +prompt(`Please enter a number between 2 and 9`);
+}
+
+for(let i = 1; i <= userInput; i++){
   console.log(`${i} * ${userInput} = ${i * userInput}`)
 }
