1c7769b97ea057c36499631db7d13e2dd4af454c
Kenneth Onu <kennetho.onu@gmail.com>
2022-12-06 22:00:56 +0800

    Match the students with keyword

--- CC-2022-02-05/addstudent.js
+++ CC-2022-02-05/addstudent.js
@@ -4,3 +4,18 @@ Add a function to our code findStudent(keyword) that matches all students in the
 If one student matches the keyword, print the string <student> is enrolled.. If more than one student matches the keyword, print the string Multiple students match this keyword.. If zero students match the keyword, print the string No students match this keyword..
 Note: The match should not be case sensitive. The keyword a should match the student Alec and the keyword A should match the student Sarah.
 */
+
+let students = ["Ada", "Mama", "Papa", "Jero"];
+
+function findStudent(keyword) {
+  let newArr = students.filter((student) =>
+    student.toLowerCase().includes(keyword.toLowerCase())
+  );
+  if (newArr.length === 1) {
+    console.log(`${newArr} is enrolled.`);
+  } else if (newArr.length > 1) {
+    console.log(`Multiple students match this keyword.`);
+  } else {
+    console.log(`No students match this keyword.`);
+  }
+}
