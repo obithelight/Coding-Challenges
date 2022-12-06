c94cf266adbf78b02fee5fb20470492d5631e1c9
Kenneth Onu <kennetho.onu@gmail.com>
2022-12-06 21:59:46 +0800

    Match the students with keyword

--- CC-2022-02-05/addstudent.js
+++ CC-2022-02-05/addstudent.js
@@ -1,1 +1,6 @@
-/**/
+/*
+Add a function to our code findStudent(keyword) that matches all students in the students array whose lowercased name includes the lowercased keyword, and print a formatted string based on the number of matches as described below.
+
+If one student matches the keyword, print the string <student> is enrolled.. If more than one student matches the keyword, print the string Multiple students match this keyword.. If zero students match the keyword, print the string No students match this keyword..
+Note: The match should not be case sensitive. The keyword a should match the student Alec and the keyword A should match the student Sarah.
+*/
