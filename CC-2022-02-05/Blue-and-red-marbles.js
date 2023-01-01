24da80fa01ae390dab138374e37ad4819d5a4995
Kenneth Onu <kennetho.onu@gmail.com>
2023-01-02 03:35:00 +0800

    Number Drills Guess Blue

--- CC-2022-02-05/Blue-and-red-marbles.js
+++ CC-2022-02-05/Blue-and-red-marbles.js
@@ -8,3 +8,4 @@ the number of red marbles pulled out so far (always lower than the starting numb
 guessBlue() should return the probability of drawing a blue marble, expressed as a float. For example, guessBlue(5, 5, 2, 3) should return 0.6.
 */
 
+//Solution 
