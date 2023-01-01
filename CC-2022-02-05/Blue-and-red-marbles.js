eabe30924202f7b1967d01cbbc5770ece3192325
Kenneth Onu <kennetho.onu@gmail.com>
2023-01-02 03:34:28 +0800

    Number Drills Guess Blue

--- CC-2022-02-05/Blue-and-red-marbles.js
+++ CC-2022-02-05/Blue-and-red-marbles.js
@@ -1,1 +1,10 @@
-/**/
+/*
+You've decided to write a function, guessBlue() to help automatically calculate whether you should guess "blue" or "red". The function should take four arguments:
+
+the number of blue marbles you put in the bag to start
+the number of red marbles you put in the bag to start
+the number of blue marbles pulled out so far (always lower than the starting number of blue marbles)
+the number of red marbles pulled out so far (always lower than the starting number of red marbles)
+guessBlue() should return the probability of drawing a blue marble, expressed as a float. For example, guessBlue(5, 5, 2, 3) should return 0.6.
+*/
+
